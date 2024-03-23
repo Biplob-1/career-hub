4855677b6c5c48bca6d9282d10f5273c0e0c12ce
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-03-24 01:44:10 +0600

    feture jobs data fetch complate

--- src/components/FetureJobs/FetureJobs.jsx
+++ src/components/FetureJobs/FetureJobs.jsx
@@ -5,6 +5,9 @@ import Job from "../Job/Job";
 
 const FetureJobs = () => {
     const [jobs, setJobs] = useState([]);
+
+    // this is not the best way to load show all data
+    const [dataLength, setDataLength] = useState(4);
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -17,12 +20,15 @@ const FetureJobs = () => {
                 <h2 className="text-6xl">Feture Jobs: {jobs.length}</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem libero nesciunt ratione, magnam a architecto minima possimus hic nulla sapiente cupiditate saepe debitis, odit similique adipisci nostrum. Sunt, voluptate.</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
-        </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <button onClick={() => setDataLength(jobs.length)} className="btn btn-primary">Show All Data</button>
+            </div>
+        </div >
     );
 };
 
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,12 +1,32 @@
-
-
+import { CiLocationOn } from "react-icons/ci";
+import { MdAttachMoney } from "react-icons/md";
+import PropTypes from 'prop-types';
 const Job = ({ job }) => {
-    const { logo } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className="card  bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={logo} alt="Shoes" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className='px-5 py-2 font-extrabold border rounded border-[#7E90EF] mr-4'>{remote_or_onsite}</button>
+                    <button className='px-5 py-2 font-extrabold border rounded border-[#7E90EF]'>{job_type}</button>
+                </div>
+                <div className='flex gap-5'>
+                    <p className="inline-flex gap-2 items-center"><CiLocationOn /> {location}</p>
+                    <p className="inline-flex gap-2 items-center"><MdAttachMoney />{salary}</p>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
-
+Job.propTypes = {
+    job: PropTypes.object.isRequired,
+}
 export default Job;
